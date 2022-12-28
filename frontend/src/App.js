d0a60b24c41e62e6fbaf7ab9db85dff46bffa1aa
bowosh <schimpfermann@gmx.de>
2022-12-28 11:00:30 +0100

    initial header component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,9 @@
-
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Header from './components/header'
 function App() {
   return (
     <div className="App">
-      <h1> Images Gallery</h1>
+      <Header title = "Just another Web App"></Header>
     </div>
   );
 }
