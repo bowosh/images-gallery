9ed5b65771e6ce52f11cd0425a1655bc874ec0bc
bowosh <schimpfermann@gmx.de>
2022-12-30 10:32:37 +0100

    controlled search component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,24 @@
+import { useState } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Header from './components/header'
+import Search from './components/Search'
+import Radiobuttons from './components/radiobuttons'
+
+
+
 function App() {
+  const [word, setWord] =useState('');
+  const handleSearchSubmit = (e) => {
+    e.preventDefault();
+    console.log(word);
+  }
+    
   return (
     <div className="App">
       <Header title = "Just another Web App"></Header>
+      <Search word={word} setWord={setWord} handleSubmit = {handleSearchSubmit}>
+      </Search>
+
     </div>
   );
 }
