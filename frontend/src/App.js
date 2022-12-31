14e55a2e898a80697fd93fc5f1153f08e36b4283
bowosh <schimpfermann@gmx.de>
2022-12-31 19:06:35 +0100

    first api call

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,24 +2,39 @@ import { useState } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Header from './components/header'
 import Search from './components/Search'
-import Radiobuttons from './components/radiobuttons'
+import React from 'react'
 
 
+const UNSPLASH_KEY = process.env.REACT_APP_UNSPLASH_KEY;
+// const UNSPLASH_KEY = '123';
 
-function App() {
+const App = () => {
   const [word, setWord] =useState('');
   const handleSearchSubmit = (e) => {
     e.preventDefault();
-    console.log(word);
+    fetch(`https://api.unsplash.com/photos/random/?query=${word}&client_id=${UNSPLASH_KEY}`)
+      .then((res) => res.json())
+      .then((data)=> {
+        console.log("Returned from API : ");
+        console.log(data);
+      })
+      .catch((err) => {
+        console.log("Returned an error : ");
+        console.log(err);
+      })
   }
     
   return (
-    <div className="App">
+  <div> 
+
+    
+
       <Header title = "Just another Web App"></Header>
       <Search word={word} setWord={setWord} handleSubmit = {handleSearchSubmit}>
       </Search>
 
     </div>
+
   );
 }
 
