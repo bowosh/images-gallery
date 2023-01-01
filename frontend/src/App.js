76fb3d9ab07af828320c1d09403cfb747785f418
bowosh <schimpfermann@gmx.de>
2023-01-01 14:19:03 +0100

    eslint and prettier setup

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,42 +1,40 @@
 import { useState } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import Header from './components/header'
-import Search from './components/Search'
-import React from 'react'
-
+import Header from './components/header';
+import Search from './components/Search';
+import React from 'react';
 
 const UNSPLASH_KEY = process.env.REACT_APP_UNSPLASH_KEY;
-// const UNSPLASH_KEY = '123';
 
 const App = () => {
-  const [word, setWord] =useState('');
+  const [word, setWord] = useState('');
   const handleSearchSubmit = (e) => {
     e.preventDefault();
-    fetch(`https://api.unsplash.com/photos/random/?query=${word}&client_id=${UNSPLASH_KEY}`)
+    fetch(
+      `https://api.unsplash.com/photos/random/?query=${word}&client_id=${UNSPLASH_KEY}`
+    )
       .then((res) => res.json())
-      .then((data)=> {
-        console.log("Returned from API : ");
+      .then((data) => {
+        console.log('Returned from API : ');
         console.log(data);
       })
       .catch((err) => {
-        console.log("Returned an error : ");
+        console.log('Returned an error : ');
         console.log(err);
-      })
-      setWord('')
-  }
-    
+      });
+    setWord('');
+  };
+
   return (
-  <div> 
-
-    
-
-      <Header title = "Just another Web App"></Header>
-      <Search word={word} setWord={setWord} handleSubmit = {handleSearchSubmit}>
-      </Search>
-
+    <div>
+      <Header title="Just another Web App"></Header>
+      <Search
+        word={word}
+        setWord={setWord}
+        handleSubmit={handleSearchSubmit}
+      ></Search>
     </div>
-
   );
-}
+};
 
 export default App;
--- frontend/src/components/header.js
+++ frontend/src/components/header.js
@@ -1,23 +1,14 @@
-
-import Button from 'react-bootstrap/Button';
 import Container from 'react-bootstrap/Container';
-import Form from 'react-bootstrap/Form';
-import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
-import NavDropdown from 'react-bootstrap/NavDropdown';
 
-const navbarStyle = {
-  backgroundColor: 'yellow'
-}
-
-const Header = ({title}) => {
+const Header = ({ title }) => {
   return (
-    <Navbar bg="primary" variant="dark"expand="lg">
+    <Navbar bg="primary" variant="dark" expand="lg">
       <Container fluid>
         <Navbar.Brand href="/">{title}</Navbar.Brand>
       </Container>
     </Navbar>
   );
-}
+};
 
 export default Header;
